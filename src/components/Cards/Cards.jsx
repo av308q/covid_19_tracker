36199833c30d6e0f7fc297e54ca93a2ba679c507
av308q <angelv1901@gmail.com>
2020-06-12 08:53:21 -0400

    added counter and fixxed flex box/css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup'
 
 import styles from './Cards.module.css';
 
@@ -9,34 +10,39 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
 } 
 
 return(
-        <div className={styles.container}>
-            <Grid container spacing={3} justify="center">
-            <Grid item component={Card}>
-              <CardContent>
-                  <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                  <Typography variant="h5">{confirmed.value}</Typography>
-                  <Typography color="textSecondary">REAL DATE</Typography>
-                  <Typography variant="body2">Number of active cases of COVID-19</Typography>
-              </CardContent>
-            </Grid>
-        </Grid>
-        <Grid item component={Card}>
-              <CardContent>
-                  <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                  <Typography variant="h5">REAL DATA</Typography>
-                  <Typography color="textSecondary">REAL DATE</Typography>
-                  <Typography variant="body2">Number of recoveries from COVID-19</Typography>
-              </CardContent>
-            </Grid>
-            <Grid item component={Card}>
-              <CardContent>
-                  <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                  <Typography variant="h5">REAL DATA</Typography>
-                  <Typography color="textSecondary">REAL DATE</Typography>
-                  <Typography variant="body2">Number of deaths caused COVID-19</Typography>
-              </CardContent>
-            </Grid>
-     </div>
+  <div className={styles.container}>
+    <Grid container spacing={3} justify="center">
+      <Grid item component={Card}>
+        <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography variant="h5">
+                <CountUp
+                start={0}
+                end={confirmed.value}
+              />
+            </Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+        </CardContent>
+      </Grid>
+  <Grid item component={Card}>
+        <CardContent>
+            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+        </CardContent>
+      </Grid>
+      <Grid item component={Card}>
+        <CardContent>
+            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of deaths caused COVID-19</Typography>
+        </CardContent>
+      </Grid>
+      </Grid>
+</div>
     );    
 }
 
